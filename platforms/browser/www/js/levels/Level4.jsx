20729e3deb27c45816fb5208ae7d275e245a8a7a
Jeff Killian <jeff.killian@atscale.com>
2018-06-13 08:07:58 -0700

    WIP

--- platforms/browser/www/js/levels/Level4.jsx
+++ platforms/browser/www/js/levels/Level4.jsx
@@ -8,28 +8,44 @@ import Fireworks from "fireworks-canvas"
 @inject("store") @observer
 export default class Level4 extends React.Component {
 
+
+    constructor(options){
+        super(options)
+        this.fireworks = ""
+
+
+
+    }
     componentDidMount(){
         const container = document.getElementById('container')
         const options = {
-            maxRockets: 3,            // max # of rockets to spawn
-            rocketSpawnInterval: 150, // millisends to check if new rockets should spawn
+            maxRockets: 1,            // max # of rockets to spawn
+            rocketSpawnInterval: 1000, // millisends to check if new rockets should spawn
             numParticles: 100,        // number of particles to spawn when rocket explodes (+0-10)
             explosionMinHeight: 0.2,  // percentage. min height at which rockets can explode
-            explosionMaxHeight: 0.9,  // percentage. max height before a particle is exploded
+            explosionMaxHeight: .9,  // percentage. max height before a particle is exploded
             explosionChance: 0.08     // chance in each tick the rocket will explode
         
         }
         // instantiate the class and call start
         // this returns a disposable - calling it will stop fireworks.
-        const fireworks = new Fireworks(container, options)
-        const stop = fireworks.start()
+        this.fireworks = new Fireworks(container, options)
+        const stop = this.fireworks.start()
+        document.addEventListener("click", x=>{
+            this.fireworks.fire() // fire a single rocket.
+        })
     }
+
+    
     render(){
         return(
-            <div>
-                <div id="container" className="fireworks-container">
+            <div onClick={this.props.store.currentLevelStore.incrementClicks}>
+                <div className="win-text-wrapper">
+                    <div>{this.props.store.currentLevelStore.winText}</div>
                 </div>
-                <div className="centered">You win2</div>
+                <div id="container" className="fireworks-container"></div>
+                
+                
             </div>
         )
     }
--- platforms/browser/www/js/levels/Level4Store.js
+++ platforms/browser/www/js/levels/Level4Store.js
@@ -1,4 +1,27 @@
 import { observable, action, computed } from "mobx"
 import $ from "jquery"
 export default class Level4Store{
+
+
+  @observable clicks
+  constructor(){
+    this.clicks = 0;
+  }
+
+  @action incrementClicks = () => {
+    this.clicks++
+  }
+
+  @computed get winText() {
+    if (this.clicks<1) return "You win"
+    if (this.clicks<2) return "Great Job"
+    if (this.clicks<5) return "Thanks for playing"
+    if (this.clicks<10) return "You really like clicking"
+    if (this.clicks<20) return "Haven't you had enough yet?"
+    if (this.clicks<40) return "You successfully showed me that you are a curious person"
+    if (this.clicks<70) return "Okay this is getting a little out of hand."
+    return "This is the last message. Thanks for playing!"
+
+  }
+  
 }
